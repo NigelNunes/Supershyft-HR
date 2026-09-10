714f836828f7244177c1d4255c9a5b89282b961a
NigelNunes <nigelnunes2004@gmail.com>
2026-09-10 11:50:27 +0530

    removed rank cards

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -70,6 +70,7 @@ export function DashboardPage() {
             kpisLoading={kpisLoading}
             rankingLoading={rankingLoading}
             selectedYear={selectedYear}
+            showRanking={false}
           />
           <MetabolicAgeDistributionCard
             categories={metabolicCategories}
